3139b529a95116e30f108490b4b4bb2557e1ad11
Nikita Viktorov <nikita.viktorov21@gmail.com>
2021-04-23 17:33:04 +0300

    feat: add shelter

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,9 @@
+const petsSlide = document.querySelectorAll(".pets__slide");
+console.log(petsSlide);
+
+function f() {
+  console.log("click for slide")
+}
+
+petsSlide.addEventListener("click", f);
+
